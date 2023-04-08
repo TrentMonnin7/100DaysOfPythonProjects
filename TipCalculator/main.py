e108a2bd2cd1421d0aff4cb6aae2cc0237b24e9f
TrentMonnin7 <trentmonnin@gmail.com>
2023-04-07 23:33:57 -0400

    complete tip calculator. Still need to debug the rounding to two digits

--- TipCalculator/main.py
+++ TipCalculator/main.py
@@ -1,5 +1,24 @@
 #prompt the user for the bills total amount
-total=input("What was the total bill?")
+total=input("What was the total bill? ")
+
+#convert total to floating point value
 new_total=float(total)
-print(type(new_total))
+
+#prompt user for tip amount
+tip=input("What percentage tip would you like to give? 10, 12, or 15? ")
+#convert tip to float
+new_tip=float(tip)
+
+#calculate the total after the tip is applied. 
+total_after_tip = new_total * (new_tip/100 + 1)
+
+#calculate final after the bill and tip are split
+friends=input("How many people to split the bill? ")
+
+
+new_friends=float(friends)
+
+final=total_after_tip/new_friends
+
+print(round(final, 3))
 
