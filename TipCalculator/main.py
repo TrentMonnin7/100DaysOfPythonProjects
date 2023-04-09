3bf2366ff8480eec516c59ccc4899a9268fe47a5
TrentMonnin7 <trentmonnin@gmail.com>
2023-04-09 19:17:32 -0400

    optimize code by using less lines. Conversion and prompts done in same line

--- TipCalculator/main.py
+++ TipCalculator/main.py
@@ -1,26 +1,20 @@
 #prompt the user for the bills total amount
-total=input("What was the total bill? ")
+total=float(input("What was the total bill? "))
 
-#convert total to floating point value
-new_total=float(total)
 
 #prompt user for tip amount
-tip=input("What percentage tip would you like to give? 10, 12, or 15? ")
-#convert tip to float
-new_tip=float(tip)
+tip=float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+
 
 #calculate the total after the tip is applied. 
-total_after_tip = new_total * (new_tip/100 + 1)
+total_after_tip = total * (tip/100 + 1)
 
 #calculate final after the bill and tip are split
-friends=input("How many people to split the bill? ")
+friends=float(input("How many people to split the bill? "))
 
 
-new_friends=float(friends)
+final=total_after_tip/friends
 
-final=total_after_tip/new_friends
+print(round(final, 2))
 
-new_final = round(final, 3)
-
-print(new_final)
 
