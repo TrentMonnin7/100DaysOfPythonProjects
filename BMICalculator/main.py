562a3e8af0377148199570b7b8a5870d5cb5aad3
TrentMonnin7 <trentmonnin@gmail.com>
2023-04-09 22:03:49 -0400

    setup BMI calculator and get basics in. Still need to debug type error

---
+++ BMICalculator/main.py
@@ -0,0 +1,19 @@
+#Prompt the user for a weight and height
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+
+#perform calculation of the BMI
+BMI=float(round(weight/height**2))
+
+#Conditionals to display what the user's BMI is
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI > 18.5 & BMI < 25:
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI > 25 & BMI < 30:
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI > 30 & BMI < 35:
+    print(f"Your BMI is {BMI}, you are obese.")
+else:
+    print(f"Your BMI is {BMI}, you are clinically obese.")
